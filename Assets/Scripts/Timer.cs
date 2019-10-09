6d2cdb7d45ba65f2432e23bbc778c5d518532e01
Siddharth Rathore <41838184+SidtehDuck@users.noreply.github.com>
2019-10-09 17:57:46 +0530

    Timer Added

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text counterText;
+    public float seconds, minutes;
+
+    void Start()
+    {
+        counterText = GetComponent<Text>() as Text;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        minutes = (int)(Time.time / 60f);
+        seconds = (int)(Time.time % 60f);
+        counterText.text = "Time: " + minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
